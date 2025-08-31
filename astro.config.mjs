70247d2cea95f9b68632336448aaa0dda6388997
Gaetano Coppoletta <tano.coppoletta@gmail.com>
2025-08-30 18:46:39 -0700

    Fix paths for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://gaetano-coppoletta.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/Tano-Coppoletta.github.io' : '',
+  base: '',
   trailingSlash: 'always',
   build: {
     assets: '_assets'
